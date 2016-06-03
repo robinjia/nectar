b6b0dc99e162cf0d123668066db9af0069fd5bab
Robin Jia <robinjia@stanford.edu>
2016-06-03 11:04:47 -0700

    Fix bug in vocabulary.py

--- nectar/vocabulary.py
+++ nectar/vocabulary.py
@@ -11,25 +11,25 @@ class Vocabulary(object):
     Args:
       unk_threshold: words with <= this many counts will be considered <UNK>.
     """
-    self.unk_threshold = 0
+    self.unk_threshold = unk_threshold
     self.counts = collections.Counter()
     self.word2index = {UNK_TOKEN: UNK_INDEX}
-    self.index2word = [UNK_TOKEN]
+    self.word_list = [UNK_TOKEN]
 
   def add_word(self, word, count=1):
     """Add a word (may still map to UNK if it doesn't pass unk_threshold)."""
     self.counts[word] += count
     if word not in self.word2index and self.counts[word] > self.unk_threshold:
-      index = len(self.index2word)
+      index = len(self.word_list)
       self.word2index[word] = index
-      self.index2word.append(word)
+      self.word_list.append(word)
 
   def add_word_hard(self, word):
     """Add word, make sure it is not UNK."""
     self.add_word(word, count=(self.unk_threshold+1))
 
   def get_word(self, index):
-    return self.index2word[index]
+    return self.word_list[index]
 
   def get_index(self, word):
     if word in self.word2index:
